b25430990fce63db92ba1fd1adcada17952a09b3
Olivier Barbeau - Rymint <olivier.barbeau@gmail.com>
2022-12-17 19:41:12 +0100

    update clojure version

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clodiuno "0.0.4-SNAPSHOT"
   :description "Clojure API for Arduino."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [rxtx22 "1.0.6"]])
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [rxtx22 "1.0.6"]]
+  :repl-options {:init-ns clodiuno.firmata})
