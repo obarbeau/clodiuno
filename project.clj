19d45242da3d31171a808416488b573743ef50a8
Olivier Barbeau - Rymint <olivier.barbeau@gmail.com>
2022-12-17 20:27:14 +0100

    use deps instead of Leiningen

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject clodiuno "0.0.4-SNAPSHOT"
-  :description "Clojure API for Arduino."
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [rxtx22 "1.0.6"]]
-  :repl-options {:init-ns clodiuno.firmata})
